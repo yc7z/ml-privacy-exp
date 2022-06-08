d6548e7a40ed66a772ce90b9bebbaf3701237f57
yuchongz.zhang <y.zhang@mel-15.syslab.sandbox>
2022-06-08 15:58:28 -0400

    added shell script for vanilla training

---
+++ vanilla.sh
@@ -0,0 +1,12 @@
+VENV_PATH="venv/bin/activate"
+
+if [ -d $VENV_PATH ] && echo $VENV_PATH
+then
+    source $VENV_PATH
+fi
+
+python ./private_compress_exp.py \
+    --lr 0.004 \
+    --batch_size 60 \
+    --epochs 15 \
+    --max_grad_norm 1.0 
